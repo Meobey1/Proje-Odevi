442380d716ed591d7e7c2e1105d423d6711dd256
Meobey1 <63191523+Meobey1@users.noreply.github.com>
2022-12-19 19:57:39 +0300

    Add files via upload

---
+++ Kitap.cs
@@ -0,0 +1,12 @@
+﻿namespace KitapUygulaması.Models
+{
+    public class Kitap
+    {
+        private int Id { get; set; }
+        public string KitapAd { get; set; }    
+        public string Yazar { get; set; }
+        public int SayfaSayisi { get; set; }
+        public int Puan { get; set; }
+
+    }
+}
